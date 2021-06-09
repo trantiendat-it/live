8ddadaa33232c66f78c1a077f05c0598681964c6
Trần Tiến Đạt <56584282+trantiendat-it@users.noreply.github.com>
2021-06-09 16:24:25 +0700

    Create src.js

---
+++ asset/js/src.js
@@ -0,0 +1,10 @@
+var player = new Playerjs({
+    id: "ttd1",
+poster:"https://truyenhinh-trantiendat.github.io/live/asset/upload/img/PicsArt_04-23-11.06.40.png",
+    file: "https://truyenhinh-trantiendat.github.io/live/asset/txt/playlist.txt"
+  });
+var player = new Playerjs({
+    id: "ttd2",
+poster:"https://truyenhinh-trantiendat.github.io/live/asset/upload/img/PicsArt_04-23-11.06.40.png",
+    file: "https://truyenhinh-trantiendat.github.io/live/asset/txt/playlist.txt"
+  });
